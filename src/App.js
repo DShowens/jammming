f912fcca39fd73d96d1e5d7bc26b396094d8be82
DShowens <donshowens@gmail.com>
2024-05-04 17:50:10 -0400

    cleared everything to start the new Jammming application

--- src/App.js
+++ src/App.js
@@ -1,40 +1,9 @@
 import React from 'react';
-import Navbar from './Navbar';
-import Home from './Home';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Create from './Create';
-import BlogDetails from './BlogDetails';
-import NotFound from './NotFound';
-
-// Router is used to wrap the entire JSX return code.
-// Switch is located at the area below the Navbar to change just the body content
-// Route is for each page change location (example: Home, Contact, FAQ, etc.)
-    // Add "exact" (prop) before "path" to avoid "/" picking up for "/create"
 
 function App() {
 
   return (
-    <Router>  
-      <div className="App">
-        <Navbar />
-        <div className="content">
-          <Switch>
-            <Route exact path="/"> 
-              <Home />
-            </Route>
-            <Route path="/create">
-              <Create />
-            </Route>
-            <Route path="/blogs/:id">
-              <BlogDetails />
-            </Route>
-            <Route path="*">
-              <NotFound />
-            </Route>
-          </Switch>
-        </div>
-      </div>
-    </Router>
+   <h1>NEW START</h1>
   );
 }
 
